633ffe0457e2648f1d21582425e3a96cb28b8812
cutegyuseok <yigyuseok@gmail.com>
2022-10-21 01:07:40 +0900

    20221020

---
+++ java_ingang/ch13/src/Member.java
@@ -0,0 +1,64 @@
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Member implements Comparable<Member> {
+    private int memberID;
+    private String memberName;
+
+    public Member(int memberID, String memberName){
+        this.memberID =memberID;
+        this.memberName = memberName;
+    }
+
+    public int getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(int memberID) {
+        this.memberID = memberID;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberID=" + memberID +
+                ", memberName='" + memberName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Member){
+            Member member = (Member) o;
+            if (this.memberID == member.memberID){
+                return true;
+            }
+            else return false;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return memberID;
+    }
+
+    @Override
+    public int compareTo(Member member) {
+        if (this.memberName.charAt(0)>member.memberName.charAt(0)){
+            return 1;
+        } else if (this.memberName.charAt(0)<member.memberName.charAt(0)) {
+            return -1;
+        }
+        else return 0;
+    }
+}
+

4957e2c4fdb547e4cbec8c3e7a1879817bb6ef59
cutegyuseok <yigyuseok@gmail.com>
2022-10-25 20:43:34 +0900

    fix bugs

--- java_ingang/ch14/src/Main.java
+++ java_ingang/ch14/src/Main.java
@@ -1,29 +1,29 @@
-import java.util.HashMap;
-
-public class Main {
-    public static void main(String[] args) {
-        MemberHashMap memberHashMap = new MemberHashMap();
-        Member memberHong = new Member(1004,"홍길동");
-        Member memberLee = new Member(1001, "이순신");
-        Member memberKim = new Member(1002, "김유신");
-        Member memberKang = new Member(1003, "강감찬");
-
-        memberHashMap.addMember(memberLee);
-        memberHashMap.addMember(memberKim);
-        memberHashMap.addMember(memberKang);
-        memberHashMap.addMember(memberHong);
-        memberHashMap.showAllMember();
-
-        HashMap<Integer , String> hashMap = new HashMap<Integer , String>();
-        hashMap.put(1001 , "kim");
-        hashMap.put(1002 , "lee");
-        hashMap.put(1003 , "him");
-        hashMap.put(1004 , "tim");
-
-        System.out.println(hashMap);
-
-
-
-
-    }
-}
+//import java.util.HashMap;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        MemberHashMap memberHashMap = new MemberHashMap();
+//        Member memberHong = new Member(1004,"홍길동");
+//        Member memberLee = new Member(1001, "이순신");
+//        Member memberKim = new Member(1002, "김유신");
+//        Member memberKang = new Member(1003, "강감찬");
+//
+//        memberHashMap.addMember(memberLee);
+//        memberHashMap.addMember(memberKim);
+//        memberHashMap.addMember(memberKang);
+//        memberHashMap.addMember(memberHong);
+//        memberHashMap.showAllMember();
+//
+//        HashMap<Integer , String> hashMap = new HashMap<Integer , String>();
+//        hashMap.put(1001 , "kim");
+//        hashMap.put(1002 , "lee");
+//        hashMap.put(1003 , "him");
+//        hashMap.put(1004 , "tim");
+//
+//        System.out.println(hashMap);
+//
+//
+//
+//
+//    }
+//}

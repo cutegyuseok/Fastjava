9a38415ed07978c9b529abf3caf749b40c5350c2
cutegyuseok <yigyuseok@gmail.com>
2022-10-25 20:43:24 +0900

    feat : lottery calc developed

---
+++ java_ingang/lotteryCalc/src/Main.java
@@ -0,0 +1,57 @@
+
+
+public class Main {
+    static int[] arr = new int[6];
+    public static void main(String[] args) {
+
+
+
+        int count = 0;
+        boolean status = false;
+        while(!status) {
+            int j=0;
+            arr = new int[6];
+            for (int i = 0; i < 6; i++) {
+                int num = (int) (Math.random() * 100);
+                if (num < 46 && num > 0 && overlap(num)) {
+                    arr[j] = num;
+                    j++;
+                } else {
+                    i--;
+                }
+            }
+            count++;
+            status = victory(arr);
+        }
+        for ( int a : arr) {
+            System.out.println(a);
+        }
+        System.out.println(count);
+
+    }
+
+    public static boolean overlap(int num){
+        boolean check = true;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]==num){
+                check =false;
+            }
+        }
+        return check;
+    }
+
+    public static boolean victory(int[] result){
+        int grade =0;
+        int[] VictoryNum ={7,16,24,27,37,44}; //로또 번호
+        for(int i =0;i<VictoryNum.length;i++){
+            for(int a :result ){
+                if(a == VictoryNum[i]){
+                    grade++;
+                }
+            }
+        }
+        if (grade==6){
+            return true;
+        }else return false;
+    }
+}

4154832cfb441a29971dfdc2eef7eb6cc8165d87
cutegyuseok <yigyuseok@gmail.com>
2022-10-27 23:00:09 +0900

    feat: 저녁 메뉴 추천기 만들기. 저녁 메뉴 삭제 method, 예외처리 완성 저녁 메뉴 수정 삭제

--- java_ingang/Games/src/dinnerMenu.java
+++ java_ingang/Games/src/dinnerMenu.java
@@ -31,100 +31,90 @@ public class dinnerMenu {
             JLabel jLabel = new JLabel("메뉴를 추천 받으시려면 버튼을 눌러주세요");
             JLabel jLabel2 = new JLabel("메뉴를 추가 하시려면 버튼을 눌러주세요");
             JLabel jLabel3 = new JLabel("메뉴를 삭제 하시려면 버튼을 눌러주세요");
-            JLabel jLabel4 = new JLabel("메뉴를 수정 하시려면 버튼을 눌러주세요");
             JLabel jLabel5 = new JLabel("메뉴목록을 보시려면 버튼을 눌러주세요");
             JButton jButton = new JButton("메뉴 추천받기");
             JButton jButton2 = new JButton("메뉴 추가하기");
             JButton jButton3 = new JButton("메뉴 삭제하기");
-            JButton jButton4 = new JButton("메뉴 수정하기");
             JButton jButton5 = new JButton("메뉴 목록보기");
             JButton jButton6 = new JButton("종료");
-            jButton.addActionListener(new ActionListener()
-                                      {@Override
-                                          public void actionPerformed(ActionEvent e) {
-                                              jd.setVisible(false);
-                                          try {
-                                              suggestMenu();
-                                          } catch (IOException ex) {
-                                              throw new RuntimeException(ex);
-                                          }
-                                          System.exit(0);
-                                          }
-                                      }
-            );
-            jButton2.addActionListener(new ActionListener()
-                                      {@Override
-                                      public void actionPerformed(ActionEvent e) {
-                                          jd.setVisible(false);
-                                          try {
-                                              addMenu();
-                                          } catch (IOException ex) {
-                                              throw new RuntimeException(ex);
-                                          }
-                                          try {
-                                              mainMenu();
-                                          } catch (IOException ex) {
-                                              throw new RuntimeException(ex);
-                                          }
-                                      }
-                                      }
-            );
-            jButton3.addActionListener(new ActionListener()
-                                       {@Override
-                                       public void actionPerformed(ActionEvent e) {
-                                           jd.setVisible(false);
-                                           deleteMenu();
-                                           try {
-                                               mainMenu();
-                                           } catch (IOException ex) {
-                                               throw new RuntimeException(ex);
-                                           }
-                                       }
-                                       }
-            );
-            jButton4.addActionListener(new ActionListener()
-                                       {@Override
-                                       public void actionPerformed(ActionEvent e) {
-                                           jd.setVisible(false);
-                                           deleteMenu();
-                                           try {
-                                               mainMenu();
-                                           } catch (IOException ex) {
-                                               throw new RuntimeException(ex);
-                                           }
-                                       }
+            jButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    jd.setVisible(false);
+                    try {
+                        suggestMenu();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    try {
+                        mainMenu();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+            });
+            jButton2.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    jd.setVisible(false);
+                    try {
+                        addMenu();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    try {
+                        mainMenu();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
+            jButton3.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    jd.setVisible(false);
+                    try {
+                        deleteMenu();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    try {
+                        mainMenu();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
                                        }
             );
-            jButton5.addActionListener(new ActionListener()
-                                       {@Override
-                                       public void actionPerformed(ActionEvent e) {
-                                           jd.setVisible(false);
-                                           JFrame jFrame = new JFrame();
-                                           JOptionPane.showMessageDialog(jFrame, showMenu());
-                                           try {
-                                               mainMenu();
-                                           } catch (IOException ex) {
-                                               throw new RuntimeException(ex);
-                                           }
-                                       }
-                                       }
-            );
-            jButton6.addActionListener(new ActionListener()
-                                       {@Override
-                                       public void actionPerformed(ActionEvent e) {
-                                           jd.setVisible(false);
-                                           System.exit(0);
-                                       }
-                                       }
-            );
+            jButton5.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    jd.setVisible(false);
+                    JFrame jFrame = new JFrame();
+                    JOptionPane.showMessageDialog(jFrame, showMenu());
+                    try {
+                        mainMenu();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+            });
+            jButton6.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    jd.setVisible(false);
+                    System.exit(0);
+                }
+
+            });
             jd.add(jLabel);
             jd.add(jButton);
             jd.add(jLabel2);
             jd.add(jButton2);
             jd.add(jLabel3);
             jd.add(jButton3);
-            jd.add(jLabel4);
-            jd.add(jButton4);
             jd.add(jLabel5);
             jd.add(jButton5);
             jd.add(jButton6);
@@ -186,36 +176,60 @@ public class dinnerMenu {
         }
         JFrame jFrame = new JFrame();
         String getMessage = JOptionPane.showInputDialog(jFrame, "추가하실 음식을 입력해주세요.");
-        fw.write(getMessage+"\n");
-        JOptionPane.showMessageDialog(jFrame, getMessage+"을 추가 했습니다.");
+        if (getMessage.isEmpty()||getMessage.equals(" ")||getMessage.equals("  ")){
+            JOptionPane.showMessageDialog(jFrame,  "음식 추가를 취소합니다.");
+        } else if(getMessage!=null) {
+            fw.write(getMessage + "\n");
+            JOptionPane.showMessageDialog(jFrame, getMessage + "을 추가 했습니다.");
+        }else {
+            JOptionPane.showMessageDialog(jFrame,  "음식 추가를 취소합니다.");
+        }
         fw.close();
         readMenuList();
     }
-    static void deleteMenu(){
+    static void deleteMenu() throws IOException {
         JFrame jFrame = new JFrame();
-
-        JDialog jd = new JDialog(jFrame);
-
-        jd.setLayout(new FlowLayout());
-        int targetNum =-1;
-
-        jd.setBounds(500, 300, 400, 300);
-        JLabel[] jLabel = new JLabel[menuNum];
-        JButton[] jButton = new JButton[menuNum];
-        for(int i=0;i<menuNum;i++) {
-            jLabel[i] = new JLabel(menus[i]);
-            jButton[i] = new JButton("삭제");
-            jButton[i].addActionListener(new ActionListener() {
-                                          @Override
-                                          public void actionPerformed(ActionEvent e) {
-                                              jd.setVisible(false);
-                                          }
-                                      }
-
-            );
-            jd.add(jLabel[i]);
-            jd.add(jButton[i]);
+        String getMessage = JOptionPane.showInputDialog(jFrame, showMenu()+
+                "\n===============\n"+"삭제하실 음식의 이름을 입력해주세요.");
+        try {
+            if (getMessage.isEmpty() || getMessage.equals(" ") || getMessage.equals("  ") || getMessage == null) {
+                JOptionPane.showMessageDialog(jFrame, "음식 추가를 취소합니다.");
+            } else if (getMessage != null) {
+                boolean status = false;
+                for (int i = 0; i < menuNum; i++) {
+                    if (menus[i].equals(getMessage)) {
+                        delete(i);
+                        status = true;
+                        break;
+                    }
+                }
+                if (status) {
+                    JOptionPane.showMessageDialog(jFrame, getMessage + "을 삭제 했습니다.");
+                } else {
+                    JOptionPane.showMessageDialog(jFrame, "해당 음식이 존재하지 않습니다.");
+                }
+            } else {
+                JOptionPane.showMessageDialog(jFrame, "음식 삭제를 취소합니다.");
+            }
+        }catch (NullPointerException e){
+            JOptionPane.showMessageDialog(jFrame, "음식 삭제를 취소합니다.");
         }
+    }
+    static void delete(int target) throws IOException {
+        FileWriter fw = new FileWriter("dinnerMenuList.txt");
+        for (int i = 0; i < target; i++) {
+            fw.write(menus[i] + "\n");
+        }
+        for (int i = target+1; i < menuNum; i++) {
+            fw.write(menus[i] + "\n");
+        }
+        fw.close();
+        if(target<yesterDayMenu){
+            yesterDayMenu--;
+        }else if(target == yesterDayMenu){
+            yesterDayMenu = 100;
+        }
+
     }
 
     static String showMenu(){
@@ -225,6 +239,5 @@ public class dinnerMenu {
             }
             return str;
     }
-
 
 }
